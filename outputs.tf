a85790f1b02d0e06e2a73d732cbfc30c9b773d1d
Christian Köberl <christian.koeberl@porscheinformatik.com>
2020-11-05 13:07:31 +0100

    Output kube_admin_config instead of kube_config

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,29 @@
+output "kube_admin_config" {
+  value = azurerm_kubernetes_cluster.main.kube_admin_config[0]
+}
+
 output "client_key" {
-  value = azurerm_kubernetes_cluster.main.kube_config[0].client_key
+  value = azurerm_kubernetes_cluster.main.kube_admin_config[0].client_key
 }
 
 output "client_certificate" {
-  value = azurerm_kubernetes_cluster.main.kube_config[0].client_certificate
+  value = azurerm_kubernetes_cluster.main.kube_admin_config[0].client_certificate
 }
 
 output "cluster_ca_certificate" {
-  value = azurerm_kubernetes_cluster.main.kube_config[0].cluster_ca_certificate
+  value = azurerm_kubernetes_cluster.main.kube_admin_config[0].cluster_ca_certificate
 }
 
 output "host" {
-  value = azurerm_kubernetes_cluster.main.kube_config[0].host
+  value = azurerm_kubernetes_cluster.main.kube_admin_config[0].host
 }
 
 output "username" {
-  value = azurerm_kubernetes_cluster.main.kube_config[0].username
+  value = azurerm_kubernetes_cluster.main.kube_admin_config[0].username
 }
 
 output "password" {
-  value = azurerm_kubernetes_cluster.main.kube_config[0].password
+  value = azurerm_kubernetes_cluster.main.kube_admin_config[0].password
 }
 
 output "node_resource_group" {
