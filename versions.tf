00209c31549f538cdad93d1601eb5a2f4ee33b4a
Christian Köberl <christian.koeberl@porscheinformatik.com>
2021-02-24 22:02:29 +0100

    Loosen version constraints

--- versions.tf
+++ versions.tf
@@ -2,10 +2,10 @@ terraform {
   required_version = ">= 0.12"
   required_providers {
     azurerm = {
-      version = "~> 2.48.0"
+      version = ">= 2.48.0"
     }
     kubernetes = {
-      version = "~> 2.0.0"
+      version = ">= 2.0.0"
     }
   }
 }
